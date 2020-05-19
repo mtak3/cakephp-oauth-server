3f164de4c45b82706ef2dd5d72072a49c4d96c3a
mtak3 <mtak@tyrellsys.com>
2020-05-19 16:28:19 +0900

    use cakephp/authentication plugin identifier method

--- src/Controller/OAuthController.php
+++ src/Controller/OAuthController.php
@@ -96,7 +96,7 @@ class OAuthController extends AppController
 
             $this->dispatchEvent('OAuthServer.beforeAuthorize', [$authRequest]);
 
-            $userId = $this->Authentication->getIdentityData($this->OAuth->getUserIdentityPath());
+            $userId = $this->Authentication->getIdentity()->getIdentifier();
             if ($userId) {
                 $authRequest->setUser(new User($userId));
             }
